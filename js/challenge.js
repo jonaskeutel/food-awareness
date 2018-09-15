39a1772a85bda7c6b1d8c5d2d1b3ef264e157a11
Jonas Keutel <jonas.keutel@student.hpi.de>
2018-09-15 21:39:11 +0200

    preliminary challenge ending screen

---
+++ js/challenge.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  var ctx = document.getElementById("myChart");
+  Chart.defaults.global.animation.duration = 5000;
+  var data = {
+      labels: ["", "",""],
+      datasets: [
+        {
+          label: "Points gained",
+          backgroundColor: ["#3e95cd", "#8e5ea2","ffffff"],
+          data: [247,126,0]
+        }
+      ]
+  };
+  var options = {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: 'Points gained in the Fridge Challenge'
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+  };
+  var myBarChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: data,
+      options: options
+  });
+});
